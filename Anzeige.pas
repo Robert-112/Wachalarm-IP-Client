fdd5769ee4dc6a5c51e330bcbf0551e1775e791f
Robert Richter <robertrichter87@web.de>
2017-02-15 19:10:02 +0100

    "Ping"-Funktion hinzugefügt

--- Anzeige.pas
+++ Anzeige.pas
@@ -6,11 +6,26 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics,
-  Dialogs, StdCtrls, ExtCtrls, blcksock;
+  Dialogs, StdCtrls, ExtCtrls, pingsend;
+
+{ Ping-Thread Deklaration }
 
 Type
+  Ping_TShowStatusEvent = procedure(Ping_Status: Boolean) of Object;
+  Ping_Thread = class(TThread)
+  private
+    Ping_Status: Boolean;
+    Ping_IP: String;
+    Ping_FOnShowStatus: Ping_TShowStatusEvent;
+    procedure ShowStatus;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(CreateSuspended: boolean);
+    property Ping_OnShowStatus: Ping_TShowStatusEvent read Ping_FOnShowStatus write Ping_FOnShowStatus;
+  end;
 
-  { TFrm_Anzeige }
+{ TFrm_Anzeige Deklaration }
 
   TFrm_Anzeige = class(TForm)
     I_Alarmbild: TImage;
@@ -49,15 +64,18 @@ Type
     procedure Nur_Zeit_anzeigen;
     procedure Zeit_und_Infotext_anzeigen(Minuten: integer; Infotext: String);
     procedure Uhr_verschieben;
+    procedure DoPing;
   private
     { private declarations }
+    MyPing_Thread: Ping_Thread;
+    procedure Ping_ServerStatus(Status: Boolean);
   public
     { public declarations }
   end; 
 
 var
   Frm_Anzeige: TFrm_Anzeige;
-  Old_Screen_Width, Old_Screen_Height, Connect_Error: Integer;
+  Old_Screen_Width, Old_Screen_Height, Connect_Error, Ping_Timer: Integer;
   Slash, Dir: String;
 
 implementation
@@ -73,9 +91,11 @@ uses
 
 procedure TFrm_Anzeige.FormCreate(Sender: TObject);
 var MonitorNr: integer;
-
 begin
-  Connect_Error := 0;
+  // Counter für Verbindungsfehler, bei start auf 1 nicht auf 0
+  Connect_Error := -1;
+  // Counter für Ping-Interval
+  Ping_Timer := 0;
   //Anzeige über ganzen Monitor ausrichten
   MonitorNr := Frm_Main.SP_Alarmbild_Monitor.Value - 1;
   Frm_Anzeige.Left := Screen.Monitors[MonitorNr].Left;
@@ -85,14 +105,14 @@ begin
   Old_Screen_Height := Screen.Height;
   Frm_Anzeige.Width := Old_Screen_Width;
   Frm_Anzeige.Height := Old_Screen_Height;
-  //Schriftart DejaVu Sans Mono setzen, sofern vorhanden, sonst default
+  // Schriftart DejaVu Sans Mono setzen, sofern vorhanden, sonst default
   if Screen.Fonts.IndexOf('DejaVu Sans') <> -1 then
   begin
     L_Clock.Font.Name := 'DejaVu Sans';
     L_LongDate.Font.Name := 'DejaVu Sans';
     L_Infotext.Font.Name := 'DejaVu Sans';
   end;
-  //Uhrzeit ermitteln und anzeigen
+  // Uhrzeit ermitteln und anzeigen
   DatumUhrzeit_setzen;
   Nur_Zeit_anzeigen;
   Uhr_verschieben;
@@ -119,7 +139,7 @@ begin
   // Uhrzeit setzen
   Frm_Anzeige.L_Clock.Caption := FormatDateTime('hh:nn:ss', now);
   // Datum setzen, falls Verbindung Okay
-  if Connect_Error < 4 then
+  if Connect_Error < 6 then
   begin
     if Frm_Anzeige.L_LongDate.Caption <> FormatDateTime('dddd, d. mmmm', now) then
     begin
@@ -130,9 +150,12 @@ begin
     end;
   end
   else
+  // sonst Fehlermeldung anzeigen
   begin
     Frm_Anzeige.L_LongDate.Caption := #$E2#$9A#$A0 + ' Verbindung prüfen ' + #$E2#$9A#$A0;
     Frm_Anzeige.L_LongDate.Font.Color := clRed;
+    L_LongDate.AdjustFontForOptimalFill;
+    L_LongDate.AdjustSize;
   end;
 end;
 
@@ -188,6 +211,52 @@ begin
   // Panel per Zufall neu ausrichten
   Panel_AllePanels.Top := Random(tmp1);
   Panel_AllePanels.Left := Random(tmp2);
+end;
+
+procedure TFrm_Anzeige.DoPing;
+begin
+  // Ping-Timer erhöhen, damit Ping nur alle 10 Sekunden ausgeführt wird
+  Ping_Timer := Ping_Timer + 1;
+  // Ping alle 10 Sekunden
+  if (Ping_Timer = 10) then
+  begin
+    // aber nur falls Server-IP bekannt
+    if not (Frm_Main.L_Server_IP.Caption = '') and not (Frm_Main.L_Server_IP.Caption = 'unbekannt') then
+    begin
+      // Thread für Ping erzeugen
+      MyPing_Thread := Ping_Thread.Create(True);
+      if Assigned(MyPing_Thread.FatalException) then
+        raise MyPing_Thread.FatalException;
+      // Variablen an Thread zuweisen
+      MyPing_Thread.Ping_OnShowStatus := @Ping_ServerStatus;
+      MyPing_Thread.Ping_IP := Frm_Main.L_Server_IP.Caption;
+      // UDP-Server starten
+      MyPing_Thread.Start;
+    end;
+    // Ping-Timer zurücksetzen
+    Ping_Timer := 0;
+  end;
+end;
+
+procedure TFrm_Anzeige.Ping_ServerStatus(Status: Boolean);
+begin
+  // Ping auswerten
+  if Status = true then
+  begin
+    Connect_Error := 0;
+    Frm_Main.L_LastServerMsg.Font.Color := clGreen;
+    Frm_Main.L_LastServerMsg.Caption := 'Verbindung OK (' + datetostr(date) + '-' + timetostr(time) + ')';
+  end
+  else
+  begin
+    // Connect-Error nur hochzählen, wenn Ping bereits einmal Okay war (es könnte ja die Firewall gesperrt sein)
+    if Connect_Error >= 0 then
+    begin
+      Connect_Error := Connect_Error + 1;
+      Frm_Main.L_LastServerMsg.Font.Color := clRed;
+      Frm_Main.L_LastServerMsg.Caption := 'Verbindung unterbrochen! (' + datetostr(date) + '-' + timetostr(time) + ')';
+    end;
+  end;
 end;
 
 procedure TFrm_Anzeige.FormKeyPress(Sender: TObject; var Key: char);
@@ -271,6 +340,7 @@ end;
 
 procedure TFrm_Anzeige.Timer_AktuelleUhrzeitTimer(Sender: TObject);
 begin
+  // Bildschirm anpassen, falls änderung in der letzten Sekunde
   if (Screen.Width <> Old_Screen_Width) or (Screen.Height <> Old_Screen_Height) then
   begin
     Old_Screen_Width := Screen.Width;
@@ -279,7 +349,10 @@ begin
     Frm_Anzeige.Height := Old_Screen_Height;
     ReCalculate_Form;
   end;
+  // Datum und Uhrzeit neu setzen
   DatumUhrzeit_setzen;
+  // Server anpingen
+  DoPing;
 end;
 
 procedure TFrm_Anzeige.Timer_InfotextanzeigenTimer(Sender: TObject);
@@ -291,6 +364,39 @@ end;
 procedure TFrm_Anzeige.Timer_UhrVerschiebenTimer(Sender: TObject);
 begin
   Uhr_verschieben;
+end;
+
+{==============================================================================}
+{=========== Ping_Thread ======================================================}
+{==============================================================================}
+
+constructor Ping_Thread.Create(CreateSuspended : boolean);
+begin
+  FreeOnTerminate := True;
+  inherited Create(CreateSuspended);
+end;
+
+procedure Ping_Thread.ShowStatus;
+begin
+  if Assigned(Ping_FOnShowStatus) then
+    Ping_FOnShowStatus(Ping_Status);
+end;
+ 
+procedure Ping_Thread.Execute;
+var
+  MyPing: TPingSend;
+begin
+  Ping_Status := false;
+  if Ping_IP <> '' then
+  begin
+    MyPing := TPingSend.Create;
+    try
+      Ping_Status := MyPing.Ping(Ping_IP);
+    finally
+      MyPing.Free;
+    end;
+    Synchronize(@Showstatus);
+  end;
 end;
 
 end.
